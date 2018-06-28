d986e507c94c6103908886b5a8157fb3a9858709
ingorewho <17761256463@163.com>
2018-06-28 11:29:24 +0800

    java获取对象的堆内存大小,需要执行jar文件才可以正确使用

--- java-objectsize/src/main/java/com/object/test/Main.java
+++ java-objectsize/src/main/java/com/object/test/Main.java
@@ -14,55 +14,27 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		//1.测试非基本类型对象大小
+		//1.测试包含非基本类型属性的对象大小
 		System.out.println(String.format("sizeof(new A):%s", ObjectSizeService.sizeOf(new A())));
-		System.out.println(String.format("fullsizeof(new A):%s", ObjectSizeService.fullSizeOf(new A())));
+		//2.测试包含非基本类型属性的对象大小
 		System.out.println(String.format("sizeof(new B):%s", ObjectSizeService.sizeOf(new B())));
-		System.out.println(String.format("fullsizeof(new B):%s", ObjectSizeService.fullSizeOf(new B())));
-		
-		//2.测试基本类型数组对象的大小
+		//3.测试基本类型数组对象的大小
+		System.out.println(String.format("sizeof(new int[0]):%s", ObjectSizeService.sizeOf(new int[0])));
 		System.out.println(String.format("sizeof(new int[1]):%s", ObjectSizeService.sizeOf(new int[1])));
-		System.out.println(String.format("fullsizeof(new int[1]):%s", ObjectSizeService.fullSizeOf(new int[1])));
-		System.out.println(String.format("sizeof(new int[2]):%s", ObjectSizeService.sizeOf(new int[2])));
-		System.out.println(String.format("fullsizeof(new int[2]):%s", ObjectSizeService.fullSizeOf(new int[2])));
-		System.out.println(String.format("sizeof(new int[3]):%s", ObjectSizeService.sizeOf(new int[3])));
-		System.out.println(String.format("fullsizeof(new int[3]):%s", ObjectSizeService.fullSizeOf(new int[3])));
-		
-		
-		//3.测试非基本类型数组对象的大小
-		System.out.println(String.format("sizeof(new Integer[1]):%s", ObjectSizeService.sizeOf(new Integer[1])));
-		System.out.println(String.format("fullsizeof(new Integer[1]):%s", ObjectSizeService.fullSizeOf(new Integer[1])));
-		System.out.println(String.format("sizeof(new Integer[2]):%s", ObjectSizeService.sizeOf(new Integer[2])));
-		System.out.println(String.format("fullsizeof(new Integer[2]):%s", ObjectSizeService.fullSizeOf(new Integer[2])));
-		System.out.println(String.format("sizeof(new Integer[3]):%s", ObjectSizeService.sizeOf(new Integer[3])));
-		System.out.println(String.format("fullsizeof(new Integer[3]):%s", ObjectSizeService.fullSizeOf(new Integer[3])));
-		
 		//4.测试非基本类型数组对象的大小
+		System.out.println(String.format("sizeof(new A[0]):%s", ObjectSizeService.sizeOf(new A[0])));
 		System.out.println(String.format("sizeof(new A[1]):%s", ObjectSizeService.sizeOf(new A[1])));
-		System.out.println(String.format("fullsizeof(new A[1]):%s", ObjectSizeService.fullSizeOf(new A[1])));
-		System.out.println(String.format("sizeof(new A[2]):%s", ObjectSizeService.sizeOf(new A[2])));
-		System.out.println(String.format("fullsizeof(new A[2]):%s", ObjectSizeService.fullSizeOf(new A[2])));
-		System.out.println(String.format("sizeof(new A[3]):%s", ObjectSizeService.sizeOf(new A[3])));
-		System.out.println(String.format("fullsizeof(new A[3]):%s", ObjectSizeService.fullSizeOf(new A[3])));
-		
-		//5.测试存在继承关系的非基本类型数组对象的大小
-		System.out.println(String.format("sizeof(new B[1]):%s", ObjectSizeService.sizeOf(new B[1])));
-		System.out.println(String.format("fullsizeof(new B[1]):%s", ObjectSizeService.fullSizeOf(new B[1])));
-		System.out.println(String.format("sizeof(new B[2]):%s", ObjectSizeService.sizeOf(new B[2])));
-		System.out.println(String.format("fullsizeof(new B[2]):%s", ObjectSizeService.fullSizeOf(new B[2])));
-		System.out.println(String.format("sizeof(new B[3]):%s", ObjectSizeService.sizeOf(new B[3])));
-		System.out.println(String.format("fullsizeof(new B[3]):%s", ObjectSizeService.fullSizeOf(new B[3])));
 	}
 }
 
 class A
 {
+	//基本类型
 	int a = 0;
-	String str = "123456";
 }
 
-class B extends A
+class B
 {
-	int b = 0;
-	String str1 = "12345678";
+	//引用类型
+	Integer b = 0;
 }
